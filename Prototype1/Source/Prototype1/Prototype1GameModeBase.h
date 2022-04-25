84f4ffa7f7b147d70243938bb184c581379f9076
terataichi <taichiz519@gmail.com>
2022-04-25 14:15:07 +0900

    プロジェクト初コミット スターターコンテンツは自分で入れてね

---
+++ Prototype1/Source/Prototype1/Prototype1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Prototype1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE1_API APrototype1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
