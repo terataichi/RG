7d339af676ede2775c1308c5d1c72199cb68c89e
Makonogh <1916020@s.asojuku.ac.jp>
2022-05-24 09:53:10 +0900

    aa

---
+++ Prototype1/Source/Prototype1/Public/StageObject/JumpPad.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "JumpPad.generated.h"
+
+UCLASS()
+class PROTOTYPE1_API AJumpPad : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AJumpPad();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		float JumpPower;
+	void Init(void);
+};
