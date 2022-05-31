2d7bb65934bb7e7be161bf93435b998be8fc5999
HiH-0906 <homuihomu0906@gmail.com>
2022-05-31 13:43:21 +0900

    GameLift初期化設定まで

--- AWSTest/Source/AWSTest/AWSTestGameMode.cpp
+++ AWSTest/Source/AWSTest/AWSTestGameMode.cpp
@@ -22,9 +22,55 @@ AAWSTestGameMode::AAWSTestGameMode()
 
 void AAWSTestGameMode::GameLiftInit()
 {
-	// とりあえずAmazonのSampleパクります。
 	// ServerTargetかつGameLiftが有効な場合のみ有効
 #if WITH_GAMELIFT
 	
+	// 最初にGameLiftModuleを取得する
+	FGameLiftServerSDKModule* gameLiftSdkModule = &FModuleManager::LoadModuleChecked<FGameLiftServerSDKModule>(FName("GameLiftServerSDK"));
+
+	// GameLiftとのローカル接続を確立し、通信を可能状態にする
+	gameLiftSdkModule->InitSDK();
+
+	// 新しいゲームセッションのアクティベーション要求に応答する
+	// ゲームサーバーにゲームプロパティを含むゲームセッションオブジェクト及びサーバー起動要求を送信する
+	// ゲームサーバーがプレイヤーの接続を受け入れる準備が出来たら GameLiftAPI.Session を呼び出す
+	
+	auto onGameSession = [=](Aws::GameLift::Server::Model::GameSession gameSession)
+	{
+		gameLiftSdkModule->ActivateGameSession();
+	};
+
+	FProcessParameters* params = new FProcessParameters();
+	params->OnStartGameSession.BindLambda(onGameSession);
+
+	// OnProcessTerminateコールバック。GameLiftはこのインスタンスをシャットダウンする前に呼び出す
+
+	// 単にGameLiftをシャットダウンすることを伝えているだけ
+	auto terminateGameSession = [=]()
+	{
+		gameLiftSdkModule->ProcessEnding();
+	};
+	params->OnTerminate.BindLambda(terminateGameSession);
+
+	// HealthCheckコールバック。GameLiftは約60秒ごとにこのコールバックを呼び出す。
+	// デフォルトでは自動的にtrueを返す。ゲームはオプションで以下のチェックを行うことができるらしい
+	// 依存関係のチェックを行い、その情報に基づいてステータスを報告することができる。60秒以内に応答がない場合falseが記録される
+	// なお今回は常にtrue
+	auto healthCheck = []()
+	{
+		return true;
+	};
+	params->OnHealthCheck.BindLambda(healthCheck);
+
+	// GameLiftへのport番号の通知
+	params->port = 7777;
+
+	// GameLiftに対して、ゲームセッションが終了したときにアップロードすべきファイル群の指示
+	TArray<FString> logfiles;
+	logfiles.Add(TEXT("aLogFile.txt"));
+	params->logParameters = logfiles;
+
+	// ProcessReadyを呼び出してGameLiftにこのゲームサーバーが準備完了なことを伝える
+	gameLiftSdkModule->ProcessReady(*params);
 #endif
 }
