c064aaf7906d8a2b5da48e25d37159e430135b8c
HiH-0906 <homuihomu0906@gmail.com>
2022-06-07 13:35:17 +0900

    AWSチュートリアルマーン

--- AWSTest/Source/AWSTest/AWSTest.Build.cs
+++ AWSTest/Source/AWSTest/AWSTest.Build.cs
@@ -8,6 +8,7 @@ public class AWSTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameLiftServerSDK" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "GameLiftServerSDK" ,
+																"Http","Json","JsonUtilities",});
 	}
 }
